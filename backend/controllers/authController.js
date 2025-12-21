cd2e45b1f619dbb3e0c80b1515055c572d354715
Mahajabin_Akter_Ritu <akterritu217@gmail.com>
2025-12-21 22:29:10 +0600

    admin matched

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,4 +1,5 @@
 const adminModel = require("../models/adminModel");
+const { responseReture } = require("../utils/response");
 
 class authController {
   admin_login = async (req, res) => {
@@ -6,8 +7,26 @@ class authController {
     console.log(req.body);
     try {
       const admin = await adminModel.findOne({ email }).select("+password");
-      console.log(admin);
-    } catch (error) {}
+      //console.log(admin);
+      if (admin) {
+        // if (admin.email === email && admin.password === password) {
+        //   responseReture(res, 200, {
+        //     message: "Admin Login successful",
+        //     admin,
+        //   });
+        // } else {
+        //   responseReture(res, 404, {
+        //     error: "Admin email or password is incorrect",
+        //   });
+        // }
+      } else {
+        responseReture(res, 404, {
+          error: "Admin email or password is incorrect",
+        });
+      }
+    } catch (error) {
+      responseReture(res, 500, { error: error.message });
+    }
   };
 }
 
