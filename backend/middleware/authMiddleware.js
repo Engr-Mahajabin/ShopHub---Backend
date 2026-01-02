a6415339f7ca80ec3a46448c516e6b921eb69bd2
Mahajabin_Akter_Ritu <akterritu217@gmail.com>
2026-01-03 03:27:20 +0600

    middleware added

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -1,18 +1,18 @@
 const jwt = require("jsonwebtoken");
 
-module.exports.authMiddleware = (req, res, next) => {
-  const {accessToken} = req.cookies;
+module.exports.authMiddleware = async (req, res, next) => {
+  const { accessToken } = req.cookies;
 
-  if(!accessToken){
-    return res.status(409).json({error: "Please login first"});
-  }else{
+  if (!accessToken) {
+    return res.status(409).json({ error: "Please login first" });
+  } else {
     try {
-       const decodeToken = await jwt.verify(accessToken,process.env.JWT_SECRET);
+      const decodeToken = await jwt.verify(accessToken, process.env.JWT_SECRET);
       req.role = decodeToken.role;
       req.id = decodeToken.id;
       next();
     } catch (error) {
-      return res.status(409).json({error: "Login please"});
+      return res.status(409).json({ error: "Login please" });
     }
   }
-}
+};
